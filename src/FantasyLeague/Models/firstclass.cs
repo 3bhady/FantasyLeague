fb22e417733aa380b3787db0967ff0e8c0afbaaa
mohamed ali <mohamed ali>
2016-10-21 02:50:05 +0200

    second commit

---
+++ src/FantasyLeague/Models/firstclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FantasyLeague.Models
+{
+    public class firstclass
+    {
+        public List<string> name = new List<string>
+       {
+           "ali","abdelhady","samir"
+       };
+        public int age { get; set; }
+    }
+}
