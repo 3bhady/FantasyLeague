afdb1f08d46d5c7f6ad28460ac253d106f0eab07
Ahmed <asamir97@hotmail.com>
2016-10-24 11:30:56 +0200

    mafeesh hagaa

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        FantasyLeagueContext _context;
+
+        public HomeController(FantasyLeagueContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult Index(Matches Match)
+        {
+            _context.Matches.Add(Match);
+            return View();
+        }
     }
 }
